8c1433910f0c89044919ce52aa2d7d6cbaa2046a
abdallh-ahsan <44622088+abdallh-ahsan@users.noreply.github.com>
2018-12-30 16:33:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("506776770700247040")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`Aleto , Aleto`);
 }, 30)
 })
 
