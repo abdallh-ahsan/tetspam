ed64db012aa8ab8d844e4a68141e1d878f4a3606
abdallh-ahsan <44622088+abdallh-ahsan@users.noreply.github.com>
2019-01-31 14:00:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
+var prefix = "1";
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
 
-client.on("ready", () => {
-let channel =     client.channels.get("506776770700247040")
-setInterval(function() {
-channel.send(`Aleto , Aleto`);
-}, 1000)
+client.on('ready', async() => {
+var server = "506776584741453824"; // ايدي السررفر
+var channel = "506776770700247040";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Clan x8wy**')
+    },505);
 })
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
 client.login(process.env.BOT_TOKEN);
